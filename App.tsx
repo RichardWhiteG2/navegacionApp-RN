0de26689686c052a4dd870aa9484b34c6ea1cdd0
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-02-25 20:02:19 -0600

    React Navigation - Pre-requisitos Listos

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
+//import 'react-native-gesture-handler';
 import React from 'react'
 import { Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+
 const App = () => {
   return (
-    <View>
-      <Text> Navegacion Screen</Text>
-    </View>
+
+    <NavigationContainer>
+      <View>
+        <Text> Navegacion Screen</Text>
+      </View>
+    </NavigationContainer>
+    // <View>
+    //   <Text> Navegacion Screen</Text>
+    // </View>
   )
 }
 export default App;
--- android/app/src/main/java/com/navegacionapp/MainActivity.java
+++ android/app/src/main/java/com/navegacionapp/MainActivity.java
@@ -1,4 +1,5 @@
 package com.navegacionapp;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -14,6 +15,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "navegacionApp";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
